fd5b5f59921b080ea7b2ec02b6b9868d355f9988
chiazam <54377599+chiazam@users.noreply.github.com>
2021-12-12 03:01:43 +0100

    Update products.js

--- src/pages/products/products.js
+++ src/pages/products/products.js
@@ -22,7 +22,7 @@ export class Products extends Component {
 
         super(props);
 
-        this.state = { product: undefined };
+        this.state = { product: {} };
 
         this.updateProduct = (data) => {
 
@@ -51,13 +51,85 @@ export class Products extends Component {
 
     render() {
 
-        return <section className="flex flex-col bg-white py-20 px-1/5 w-full">
+        console.log(this.state.product);
+
+        return <section className="flex flex-col bg-white py-20 px-16 w-full">
 
             <h1 className="text-indigo-900 font-semibold text-4xl font-josefin-sans self-center">Product Details</h1>
 
-            <div className="flex flex-wrap mt-4 w-full">
+            <div className="flex max-w-3xl mt-4 w-full m-auto shadow-product h-92">
 
+                <section class="flex p-1 w-3/6">
 
+                    <section class="flex w-2/6 flex-col">
+
+                        <section class="h-2/6 m-1 rounded-md border overflow-hidden">
+
+                            <img src={this.state.product.image} alt="" className="object-cover w-full h-full" />
+
+                        </section>
+
+                        <section class="h-2/6 m-1 rounded-md border overflow-hidden">
+
+                            <img src={this.state.product.image} alt="" className="object-cover w-full h-full" />
+
+                        </section>
+
+                        <section class="h-2/6 m-1 rounded-md border overflow-hidden">
+
+                            <img src={this.state.product.image} alt="" className="object-cover w-full h-full" />
+
+                        </section>
+
+                    </section>
+
+                    <section class="flex-1 m-1 rounded-md border overflow-hidden">
+
+                        <img src={this.state.product.image} alt="" className="object-cover w-full h-full" />
+
+                    </section>
+
+                </section>
+
+                <section class="flex flex-1 p-1 items-center justify-center">
+
+                    <section class="w-5/6">
+
+                        <h3 className="text-lg text-brand-blue tracking-wide font-bold font-lato"> {this.state.product.name} </h3>
+
+                        <p className="text-blue-900 text-base font-josefin-sans mt-3">
+
+                            <section className="text-brand-pink">{this.state.product.price}</section>
+
+                        </p>
+
+                        <p className="text-blue-900 text-base font-josefin-sans mt-3">
+
+                            <section className="font-bold">Category</section>
+
+                            <section><span class="text-sm">{this.state.product.category}</span></section>
+
+                        </p>
+
+                        <p className="text-blue-900 text-base font-josefin-sans mt-3">
+
+                            <section className="font-bold">Description</section>
+
+                            <section> <span class="text-sm">{this.state.product.desc}</span></section>
+
+                        </p>
+
+                        <p className="bg-violet rounded-full text-white text-base flex justify-center items-center font-josefin-sans mt-3 cursor-pointer">
+
+                            <section>Add To Cart</section>
+
+                            <section className="p-2 mx-2"> <CartSvg className="h-3.5 w-3.5 fill-current text-white"/> </section>
+
+                        </p>
+
+                    </section>
+
+                </section>
 
             </div>
 
